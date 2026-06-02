d5b62a28a8133a6686ad74e1a77ba47639001185
Lucas Branco <lucasbranco544@gmail.com>
2026-06-02 00:44:22 -0300

    refactor(percent): documenta a guarda de divisão por zero
    Comentário explicando a convenção (y=0 -> 0). Sem mudança de comportamento;
    suíte common 780 passing, percent.js 100% de cobertura.

--- website/common/script/libs/percent.js
+++ website/common/script/libs/percent.js
@@ -1,6 +1,8 @@
 // TODO move to client
 
 export default function percent (x, y, dir) {
+  // Sem total (y = 0) a porcentagem é indefinida (divisão por zero geraria
+  // Infinity/NaN); por convenção retornamos 0.
   if (y === 0) {
     return 0;
   }
